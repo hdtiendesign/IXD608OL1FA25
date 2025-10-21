295a2b7dd706947f45b532551fb2641450317aaa
Tien Tran <tientien.11299@gmail.com>
2025-10-21 00:49:26 -0700

    Convert site to PHP landing page; add parts/, products, images; update styles; remove old index.html

---
+++ parts/meta.php
@@ -0,0 +1,6 @@
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="stylesheet" href="/IXD608OL1FA25/lib/css/styleguide.css">
+	<link rel="stylesheet" href="/IXD608OL1FA25/lib/css/gridsystem.css">
+	<link rel="stylesheet" href="/IXD608OL1FA25/css/storetheme.css">
